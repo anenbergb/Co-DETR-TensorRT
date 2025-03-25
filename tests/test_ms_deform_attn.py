5296e6881a003fc0d627d3f12bb04045226eecf6
Bryan Anenberg <anenbergb@gmail.com>
2025-03-24 21:10:41 -0700

    copied tests from mmcv/blob/main/tests/test_ops/test_ms_deformable_attn.py

--- tests/test_ms_deform_attn.py
+++ tests/test_ms_deform_attn.py
@@ -1,7 +1,13 @@
 import torch
 import pytest
+from torch.amp import autocast
+from torch.autograd import gradcheck
 from codetr.ops.ms_deform_attn import ms_deform_attn_forward, ms_deform_attn_backward
-from codetr.ops.multi_scale_deform_attn import multi_scale_deformable_attn_pytorch, MultiScaleDeformableAttention
+from codetr.ops.multi_scale_deform_attn import (
+    multi_scale_deformable_attn_pytorch,
+    MultiScaleDeformableAttention,
+    MultiScaleDeformableAttnFunction,
+)
 
 
 @pytest.mark.parametrize("dtype", [torch.float32, torch.float16])
@@ -162,6 +168,260 @@ def test_MultiScaleDeformableAttention(dtype):
 
     # Check that the output is on the correct device
     assert output.device == torch.device(device)
+
+
+@pytest.mark.parametrize("device", ["cpu", "cuda:0"])
+def test_multiscale_deformable_attention(device):
+    with pytest.raises(ValueError):
+        # embed_dims must be divisible by num_heads,
+        MultiScaleDeformableAttention(
+            embed_dims=256,
+            num_heads=7,
+        )
+    device = torch.device(device)
+    msda = MultiScaleDeformableAttention(embed_dims=3, num_levels=2, num_heads=3)
+    msda.init_weights()
+    num_query = 5
+    bs = 1
+    embed_dims = 3
+    query = torch.rand(num_query, bs, embed_dims).to(device)
+    key = torch.rand(num_query, bs, embed_dims).to(device)
+    spatial_shapes = torch.Tensor([[2, 2], [1, 1]]).long().to(device)
+    level_start_index = torch.Tensor([0, 4]).long().to(device)
+    reference_points = torch.rand(bs, num_query, 2, 2).to(device)
+    msda.to(device)
+    msda(
+        query,
+        key,
+        key,
+        reference_points=reference_points,
+        spatial_shapes=spatial_shapes,
+        level_start_index=level_start_index,
+    )
+
+    # test with value_proj_ratio
+    embed_dims = 6
+    value_proj_ratio = 0.5
+    query = torch.rand(num_query, bs, embed_dims).to(device)
+    key = torch.rand(num_query, bs, embed_dims).to(device)
+    msda = MultiScaleDeformableAttention(
+        embed_dims=embed_dims, num_levels=2, num_heads=3, value_proj_ratio=value_proj_ratio
+    )
+    msda.init_weights()
+    msda.to(device)
+    msda(
+        query,
+        key,
+        key,
+        reference_points=reference_points,
+        spatial_shapes=spatial_shapes,
+        level_start_index=level_start_index,
+    )
+
+
+def test_forward_multi_scale_deformable_attn_pytorch():
+    N, M, D = 1, 2, 2
+    Lq, L, P = 2, 2, 2
+    shapes = torch.as_tensor([(6, 4), (3, 2)], dtype=torch.long)
+    S = sum((H * W).item() for H, W in shapes)
+
+    torch.manual_seed(3)
+    value = torch.rand(N, S, M, D) * 0.01
+    sampling_locations = torch.rand(N, Lq, M, L, P, 2)
+    attention_weights = torch.rand(N, Lq, M, L, P) + 1e-5
+    attention_weights /= attention_weights.sum(-1, keepdim=True).sum(-2, keepdim=True)
+
+    multi_scale_deformable_attn_pytorch(
+        value.double(), shapes, sampling_locations.double(), attention_weights.double()
+    ).detach()
+
+
+def test_forward_equal_with_pytorch_double():
+    N, M, D = 1, 2, 2
+    Lq, L, P = 2, 2, 2
+    shapes = torch.as_tensor([(6, 4), (3, 2)], dtype=torch.long)
+    level_start_index = torch.cat((shapes.new_zeros((1,)), shapes.prod(1).cumsum(0)[:-1]))
+    S = sum((H * W).item() for H, W in shapes)
+
+    torch.manual_seed(3)
+    value = torch.rand(N, S, M, D) * 0.01
+    sampling_locations = torch.rand(N, Lq, M, L, P, 2)
+    attention_weights = torch.rand(N, Lq, M, L, P) + 1e-5
+    attention_weights /= attention_weights.sum(-1, keepdim=True).sum(-2, keepdim=True)
+    im2col_step = 2
+    output_pytorch = (
+        multi_scale_deformable_attn_pytorch(
+            value.double(), shapes, sampling_locations.double(), attention_weights.double()
+        )
+        .detach()
+        .cpu()
+    )
+
+    output_cuda = (
+        MultiScaleDeformableAttnFunction.apply(
+            value.cuda().double(),
+            shapes.cuda(),
+            level_start_index.cuda(),
+            sampling_locations.cuda().double(),
+            attention_weights.cuda().double(),
+            im2col_step,
+        )
+        .detach()
+        .cpu()
+    )
+    assert torch.allclose(output_cuda, output_pytorch)
+    max_abs_err = (output_cuda - output_pytorch).abs().max()
+    max_rel_err = ((output_cuda - output_pytorch).abs() / output_pytorch.abs()).max()
+    assert max_abs_err < 1e-18
+    assert max_rel_err < 1e-15
+
+
+def test_forward_equal_with_pytorch_float():
+    device = "cuda:0"
+    N, M, D = 1, 2, 2
+    Lq, L, P = 2, 2, 2
+    shapes = torch.as_tensor([(6, 4), (3, 2)], dtype=torch.long)
+    level_start_index = torch.cat((shapes.new_zeros((1,)), shapes.prod(1).cumsum(0)[:-1]))
+    S = sum((H * W).item() for H, W in shapes)
+
+    torch.manual_seed(3)
+    value = torch.rand(N, S, M, D) * 0.01
+    sampling_locations = torch.rand(N, Lq, M, L, P, 2)
+    attention_weights = torch.rand(N, Lq, M, L, P) + 1e-5
+    attention_weights /= attention_weights.sum(-1, keepdim=True).sum(-2, keepdim=True)
+    im2col_step = 2
+    output_pytorch = (
+        multi_scale_deformable_attn_pytorch(value, shapes, sampling_locations, attention_weights).detach().cpu()
+    )
+
+    output_device = (
+        MultiScaleDeformableAttnFunction.apply(
+            value.to(device),
+            shapes.to(device),
+            level_start_index.to(device),
+            sampling_locations.to(device),
+            attention_weights.to(device),
+            im2col_step,
+        )
+        .detach()
+        .cpu()
+    )
+    assert torch.allclose(output_device, output_pytorch, rtol=1e-2, atol=1e-3)
+    max_abs_err = (output_device - output_pytorch).abs().max()
+    max_rel_err = ((output_device - output_pytorch).abs() / output_pytorch.abs()).max()
+    assert max_abs_err < 1e-9
+    assert max_rel_err < 1e-6
+
+
+def test_forward_equal_with_autocast():
+    device = "cuda:0"
+    N, M, D = 1, 2, 2
+    Lq, L, P = 2, 2, 2
+    shapes = torch.as_tensor([(6, 4), (3, 2)], dtype=torch.long)
+    level_start_index = torch.cat((shapes.new_zeros((1,)), shapes.prod(1).cumsum(0)[:-1]))
+    S = sum((H * W).item() for H, W in shapes)
+
+    torch.manual_seed(3)
+    value = torch.rand(N, S, M, D) * 0.01
+    sampling_locations = torch.rand(N, Lq, M, L, P, 2)
+    attention_weights = torch.rand(N, Lq, M, L, P) + 1e-5
+    attention_weights /= attention_weights.sum(-1, keepdim=True).sum(-2, keepdim=True)
+    im2col_step = 2
+    output_pytorch = (
+        multi_scale_deformable_attn_pytorch(value, shapes, sampling_locations, attention_weights).detach().cpu()
+    )
+
+    # float test
+    dtype = torch.float
+    with autocast("cuda", enabled=True):
+        output_device = (
+            MultiScaleDeformableAttnFunction.apply(
+                value.to(device).type(dtype),
+                shapes.to(device),
+                level_start_index.to(device),
+                sampling_locations.to(device),
+                attention_weights.to(device),
+                im2col_step,
+            )
+            .detach()
+            .cpu()
+        )
+    assert torch.allclose(output_device, output_pytorch, rtol=1e-2, atol=1e-3)
+    max_abs_err = (output_device - output_pytorch).abs().max()
+    max_rel_err = ((output_device - output_pytorch).abs() / output_pytorch.abs()).max()
+    assert max_abs_err < 1e-9
+    assert max_rel_err < 1e-6
+
+    # half test
+    dtype = torch.half
+    with autocast("cuda", enabled=True):
+        output_device = (
+            MultiScaleDeformableAttnFunction.apply(
+                value.to(device).type(dtype),
+                shapes.to(device),
+                level_start_index.to(device),
+                sampling_locations.to(device),
+                attention_weights.to(device),
+                im2col_step,
+            )
+            .detach()
+            .cpu()
+        )
+    assert torch.allclose(output_device, output_pytorch.half(), rtol=1e-2, atol=1e-3)
+    max_abs_err = (output_device - output_pytorch).abs().max()
+    max_rel_err = ((output_device - output_pytorch).abs() / output_pytorch.abs()).max()
+    assert max_abs_err < 1e-5
+    assert max_rel_err < 1e-2
+
+
+@pytest.mark.parametrize(
+    "channels",
+    [
+        4,
+        30,
+        32,
+        64,
+        71,
+        1025,
+    ],
+)
+@pytest.mark.parametrize("dtype", [torch.float, torch.double, torch.half])
+def test_gradient_numerical(channels, dtype, grad_value=True, grad_sampling_loc=True, grad_attn_weight=True):
+    device = "cuda:0"
+    N, M, _ = 1, 2, 2
+    Lq, L, P = 2, 2, 2
+    shapes = torch.as_tensor([(3, 2), (2, 1)], dtype=torch.long).to(device)
+    level_start_index = torch.cat((shapes.new_zeros((1,)), shapes.prod(1).cumsum(0)[:-1]))
+    S = sum((H * W).item() for H, W in shapes)
+
+    value = torch.rand(N, S, M, channels).to(device) * 0.01
+    sampling_locations = torch.rand(N, Lq, M, L, P, 2).to(device)
+    attention_weights = torch.rand(N, Lq, M, L, P).to(device) + 1e-5
+    attention_weights /= attention_weights.sum(-1, keepdim=True).sum(-2, keepdim=True)
+    im2col_step = 2
+
+    func = MultiScaleDeformableAttnFunction.apply
+
+    value.requires_grad = grad_value
+    sampling_locations.requires_grad = grad_sampling_loc
+    attention_weights.requires_grad = grad_attn_weight
+
+    dtype = torch.double
+    eps = 1e-6
+
+    assert gradcheck(
+        func,
+        (
+            value.to(dtype),
+            shapes,
+            level_start_index,
+            sampling_locations.to(dtype),
+            attention_weights.to(dtype),
+            im2col_step,
+        ),
+        eps=eps,
+        atol=1e-2,
+    )
 
 
 if __name__ == "__main__":
