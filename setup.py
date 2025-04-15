befc37738b5892f6b82d79edf8ed5e2038f3ec0f
Bryan Anenberg <anenbergb@gmail.com>
2025-04-14 20:37:13 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,33 @@
-import os
-from setuptools import setup, find_packages
-from torch.utils.cpp_extension import CUDAExtension, BuildExtension
-
+from setuptools import find_packages, setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
     name="codetr",
-    version="1.0.1",
+    version="1.0.2",
     url="https://github.com/anenbergb/Co-DETR-TensorRT",
     author="Bryan Anenberg",
     author_email="anenbergb@gmail.com",
     description="MMDetection Co-DETR exported to TensorRT",
     python_requires=">=3.10",
     packages=find_packages(),
-    install_requires=[
-    ],
+    install_requires=["numpy", "pytest"],
     extras_require={
         "full": [
             # tested with torch 2.6.0, torch_tensorrt 2.6.0, tensorrt 10.7.0
             "torch",
+            "torchvision",
             "torch-tensorrt",
             "tensorrt",
             # copied from mmdetection/requirements/mminstall.txt v3.3.0
             "mmcv>=2.0.0rc4,<2.2.0",
             "mmengine>=0.7.1,<1.0.0",
-            "mmdet @ git+https://github.com/open-mmlab/mmdetection.git@v3.3.0"
+            "mmdet @ git+https://github.com/open-mmlab/mmdetection.git@v3.3.0",
         ],
         "notebook": [
             "jupyter",
             "jupyter_contrib_nbextensions",
         ],
-        "dev": ["black", "mypy", "flake8", "isort", "ipdb", "pytest", "types-requests"],
+        "dev": ["black", "flake8", "isort", "ipdb"],
     },
     ext_modules=[
         CUDAExtension(
@@ -40,25 +38,18 @@ setup(
             ],
             extra_compile_args={
                 "cxx": [
-                    # "-O3",
-                    "-O0", # debug mode
+                    "-O3",
                     "-fdiagnostics-color=always",
                     "-DPy_LIMITED_API=0x03090000",  # min CPython version 3.
-                    "-g", # debug mode
                 ],
-            
                 "nvcc": [
-                    # "-O3",
-                    "-O0", # debug mode
+                    "-O3",
                     "--use_fast_math",
                     # Explicitly targeting CUDA Compute Capability 8.9 (RTX 4090)
                     "-gencode=arch=compute_89,code=sm_89",
-                    "-g", # debug mode
                 ],
             },
-            extra_link_args=['-Wl,--no-as-needed', '-lcuda',
-                             "-O0", "-g" # debug mode
-                             ],
+            extra_link_args=["-Wl,--no-as-needed", "-lcuda", "-O3"],
             py_limited_api=True,
         ),
     ],
